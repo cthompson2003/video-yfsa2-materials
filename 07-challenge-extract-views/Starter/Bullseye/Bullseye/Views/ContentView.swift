7512a44e0f40fc2da0a07a5cb98084f2cae34a15
Student <student@Empowrs-MacBook-Pro.local>
2021-06-09 22:25:57 -0700

    finish hw

--- 07-challenge-extract-views/Starter/Bullseye/Bullseye/Views/ContentView.swift
+++ 07-challenge-extract-views/Starter/Bullseye/Bullseye/Views/ContentView.swift
@@ -19,37 +19,9 @@ struct ContentView: View {
         .edgesIgnoringSafeArea(.all)
       VStack {
         InstructionsView(game: $game)
-        HStack {
-          Text("1")
-            .bold()
-            .foregroundColor(Color("TextColor"))
-          Slider(value: $sliderValue, in: 1.0...100.0)
-          Text("100")
-            .bold()
-            .foregroundColor(Color("TextColor"))
-        }
-        .padding()
-        Button(action: {
-          print("Hello, SwiftUI!")
-          alertIsVisible = true
-        }) {
-          Text("Hit me".uppercased())
-            .bold()
-            .font(.title3)
-        }
-          .padding(20.0)
-          .background(
-            ZStack {
-              Color("ButtonColor")
-              LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
-            }
-          )
-          .foregroundColor(Color.white)
-          .cornerRadius(21.0)
-          .alert(isPresented: $alertIsVisible, content: {
-            let roundedValue = Int(sliderValue.rounded())
-            return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Awesome!")))
-          })
+        SliderView(sliderValue:$sliderValue)
+        HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
+        
       }
     }
   }
@@ -67,6 +39,55 @@ struct InstructionsView: View {
     }
   }
 }
+
+
+struct SliderView: View {
+    @Binding  var sliderValue: Double
+
+  var body: some View {
+    HStack {
+      SliderLabelText(text: "1")
+      SliderLabelText(text: "100")
+    }
+    .padding()
+  }
+}
+
+
+struct HitMeButton: View {
+    
+    
+    
+    @Binding  var alertIsVisible: Bool
+    @Binding  var sliderValue : Double
+    @Binding  var game : Game
+    
+
+  var body: some View {
+    Button(action: {
+      print("Hello, SwiftUI!")
+      alertIsVisible = true
+    }) {
+      Text("Hit me".uppercased())
+        .bold()
+        .font(.title3)
+    }
+      .padding(20.0)
+      .background(
+        ZStack {
+          Color("ButtonColor")
+          LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+        }
+      )
+      .foregroundColor(Color.white)
+      .cornerRadius(21.0)
+      .alert(isPresented: $alertIsVisible, content: {
+        let roundedValue = Int(sliderValue.rounded())
+        return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Awesome!")))
+      })
+  }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
